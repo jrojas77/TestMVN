37578b16e23ad23eb6009e5803acb36171aa10cd
jrojas77 <jrojas_77@hotmail.com>
2024-03-16 15:38:47 -0400

    First Commit

---
+++ COMP367_Lab3_Q1/src/main/java/com/devops/demo/AppleDuquinalComp367Q3Application.java
@@ -0,0 +1,14 @@
+package com.devops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleDuquinalComp367Q3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleDuquinalComp367Q3Application.class, args);
+		System.out.println("Springboot starting. . .");
+	}
+
+}
